9de186cdf161b5aa1297cef7a9a5f54dede74c8a
Dermot Rossiter <dermot88@gmail.com>
2021-05-08 14:12:36 +0100

    Setup start Edit Expense

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -61,6 +61,17 @@ export const setExpenses = (expenses) => ({
 	expenses,
 });
 
+export const startEditExpense = (id, updates) => {
+	return (dispatch) => {
+		return database
+			.ref(`expenses/${id}`)
+			.update(updates)
+			.then(() => {
+				dispatch(editExpense(id, updates));
+			});
+	};
+};
+
 export const startSetExpenses = () => {
 	return (dispatch) => {
 		return database
